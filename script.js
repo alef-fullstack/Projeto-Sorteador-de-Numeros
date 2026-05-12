9769a8ff50e18fff0f50ce94e5ced2c37a82ef4b
alef-fullstack <dev.alef25@gmail.com>
2026-05-11 22:14:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateNumber(){
+
+const min = Math.ceil(document.querySelector(".input-1").value)
+const max = Math.floor(document.querySelector(".input-2").value)
+
+const result = Math.floor(Math.random() * (max - min + 1)) + min;
+const resulttext = document.querySelector(".result")
+
+resulttext.innerHTML = result
+
+}
+
