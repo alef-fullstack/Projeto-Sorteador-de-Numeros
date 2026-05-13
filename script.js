6541e6e99d609c33ec1d3f5920c64bd39e249e60
alef-fullstack <dev.alef25@gmail.com>
2026-05-12 22:06:02 -0300

    alteração no if e else

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const min = Math.ceil(document.querySelector(".input-1").value)
 const max = Math.floor(document.querySelector(".input-2").value)
 
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
-const resulttext = document.querySelector(".result")
 
-resulttext.innerHTML = result
+
+if(min >= max){
+    alert("O valor mínimo tem que ser MENOR que o valor máximo!")
+}
+else{
+    const resulttext = document.querySelector(".result")
+    resulttext.innerHTML = result
+}
 
 }
 
